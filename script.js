7ab94f58efe7a118b747bf2130171c6c8fb9257c
Bruno <brubarratella@hotmail.com>
2023-02-28 10:50:49 -0300

    Projeto pronto

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ const corRoxa = document.getElementById('btn-roxo')
 corPadrao.addEventListener("click", () => {
     document.body.style.cssText = 
     'background-color: var(--fundo-padrao);'+
-    'color: var(--cor-padrao-fonte)'
+    'color: var(--cor-padrao-fonte);'+
+    'text-shadow: 3px 3px 3px var(--fundo2);'
 })
 
 corPreta.addEventListener("click", () => {
     document.body.style.cssText = 
     'background-color: var(--fundo2);'+
-    'color: var(--cor-padrao-fonte;)'
+    'color: var(--cor-padrao-fonte);'+
+    'text-shadow: 3px 3px 3px var(--fonte2);'
 })
 
 corRoxa.addEventListener("click", () => {
     document.body.style.cssText = 
     'background-color: var(--fundo3);'+
-    'color: var(--fonte3);'
+    'color: var(--cor-padrao-fonte);'+
+    'text-shadow: 3px 3px 1px var(--fonte3);'
 })
